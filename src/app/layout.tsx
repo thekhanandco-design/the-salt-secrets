8fd85537999092b3991ba7854d744cb39eb20df5
thekhanandco <thekhanandco@gmail.com>
2026-06-10 18:18:36 +0500

    Optimized OG image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
     siteName: "The Salt Secrets",
     images: [
       {
-        url: "/og-image.png",
+        url: "/og-image.jpg",
         width: 1200,
         height: 630,
         alt: "The Salt Secrets",
