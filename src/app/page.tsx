4f46bbc52a15f40e0e7b65a4be1fb5caf643d0e1
thekhanandco <thekhanandco@gmail.com>
2026-06-14 20:02:55 +0500

    Homepage clone redesign

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,5 @@
-import Hero from "@/components/Hero";
-import PrivateLabel from "@/components/PrivateLabel";
-import Products from "@/components/Products";
-import WhyChooseUs from "@/components/WhyChooseUs";
-import Certifications from "@/components/Certifications";
-import ExportMarkets from "@/components/ExportMarkets";
+import HomepageClone from "@/components/HomepageClone";
 
 export default function Home() {
-  return (
-    <main className="bg-white">
-      {/* HERO */}
-      <Hero />
-
-      {/* PRIVATE LABEL - IMMEDIATELY AFTER HERO */}
-      <PrivateLabel />
-
-      {/* PRODUCTS */}
-      <Products />
-
-      {/* WHY CHOOSE US */}
-      <WhyChooseUs />
-
-      {/* CERTIFICATIONS */}
-      <Certifications />
-
-      {/* EXPORT MARKETS */}
-      <ExportMarkets />
-    </main>
-  );
+  return <HomepageClone />;
 }
